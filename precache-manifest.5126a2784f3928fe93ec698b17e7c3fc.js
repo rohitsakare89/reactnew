0ab1dbf73614eac54866d52b8401c7b0f03bcbd2
rohitsakare89 <rohitsakare89@gmail.com>
2020-07-11 13:20:49 +0530

    Updates

---
+++ precache-manifest.5126a2784f3928fe93ec698b17e7c3fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743b4a43de8938a92d019414a181d36a",
+    "url": "/reactnew/index.html"
+  },
+  {
+    "revision": "6a067b4976b182dfe6b6",
+    "url": "/reactnew/static/css/main.2e55a076.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/reactnew/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactnew/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a067b4976b182dfe6b6",
+    "url": "/reactnew/static/js/main.d2453007.chunk.js"
+  },
+  {
+    "revision": "553c3155685ade683b05",
+    "url": "/reactnew/static/js/runtime-main.1c05f0ef.js"
+  }
+]);
